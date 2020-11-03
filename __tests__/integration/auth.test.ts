e43c9b44b82bd204b02889fd58a8a4460156d7e4
isaquetdiniz <itd@cin.ufpe.br>
2020-11-03 17:21:09 -0300

    feat(jest): create db and test scripts

---
+++ __tests__/integration/auth.test.ts
@@ -0,0 +1,8 @@
+describe("Authentication", () => {
+  it("Should received JWT token when authenticated with valid credentials", async () => {
+    const x = 2;
+    const y = 6;
+    const sum = x + y;
+    expect(sum).toBe(8);
+  });
+});
