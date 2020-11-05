66983101b0f98f360fcfb7c8c1374cb0677dd33a
isaquetdiniz <itd@cin.ufpe.br>
2020-11-05 09:48:14 -0300

    feat(configure-client): added logout button

--- client/src/views/edit.tsx
+++ client/src/views/edit.tsx
@@ -1,23 +1,41 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import axios from "axios";
+import { Typography, Button, Modal, Space } from "antd";
 
-import {
-  Typography, Button, Modal, Space,
-} from 'antd';
+import TableProduct from "../components/TableOrder";
+import TableOrder from "../components/TableProduct";
 
-import TableProduct from '../components/TableOrder';
-import TableOrder from '../components/TableProduct';
+import FormProduct from "../components/FormProduct";
+import FormOrder from "../components/FormOrder";
+import { useAuth } from "../context/AuthContextProvider";
 
-import FormProduct from '../components/FormProduct';
-import FormOrder from '../components/FormOrder';
+import { useHistory } from "react-router-dom";
 
 const Edit: React.FC = () => {
   const [visibleNewOrder, setVisibleNewOrder] = useState(false);
   const [visibleNewProduct, setVisibleNewProduct] = useState(false);
   const { Title } = Typography;
 
+  const setToken = useAuth()[1];
+  const history = useHistory();
+
+  const logout = () => {
+    axios
+      .post("http://localhost:3001/logout")
+      .then((res) => {
+        setToken(res.data.token);
+        history.push("/login");
+      })
+      .catch(() =>
+        Modal.error({ title: "Erro ao deslogar", content: "Tente novamente" })
+      );
+  };
   return (
     <Space align="center" direction="vertical">
       <Title>Produtos e Pedidos</Title>
+      <Button type="primary" danger onClick={() => logout()}>
+        Sair
+      </Button>
       <Space direction="vertical" size="middle">
         <Title level={3}>Produtos</Title>
         <Button type="primary" onClick={() => setVisibleNewProduct(true)}>
